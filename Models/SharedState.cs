9c46394a15e5b2e87e110aec9216fac7cc7a4cc5
GOPI T <gopi45666@outlook.com>
2024-06-12 18:05:17 +0530

    change

--- Models/SharedState.cs
+++ Models/SharedState.cs
@@ -2,7 +2,8 @@
 {
     public class SharedState
     {
-        public string MessageFromTeacher { get; set; } 
-        public string MessageFromStudent1 { get; set; } 
+        public string? MessageFromTeacher { get; set; } 
+        public string? MessageFromStudent1 { get; set; }
+        public string? MessageFromStudent2 { get; set; }
     }
 }
